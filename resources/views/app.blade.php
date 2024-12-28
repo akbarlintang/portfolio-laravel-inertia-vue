29ebcbc1634dd6ec632aacf2908e7358fd335e05
akbarlintang_ <lintangajiakbar@gmail.com>
2024-12-28 18:56:32 +0700

    Add meta tag to force https

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Portfolio') }}</title>
 
+        <!-- Force HTTPS -->
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
