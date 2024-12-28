e27d41263d752d8f6b90bf5cbfaf35801d56d095
akbarlintang_ <lintangajiakbar@gmail.com>
2024-12-28 18:24:12 +0700

    Fix ziggy path to absolute path

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            ziggy: '/resources/js/ziggy',
+            ziggy: '/vendor/tightenco/ziggy',
         },
     },
 });
