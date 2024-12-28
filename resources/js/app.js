81024a85b851cc2831e2bc3c9a0898704e7a6411
akbarlintang_ <lintangajiakbar@gmail.com>
2024-12-28 18:13:37 +0700

    Update ziggy path file

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import 'lineicons/dist/lineicons.css';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from './ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
